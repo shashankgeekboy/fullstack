4b425c73b7236a384749b4f2f6ea9411be3f0e7c
Shashank_geekboy <113760650+shashankgeekboy@users.noreply.github.com>
2023-07-17 05:03:38 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,112 +1,131 @@
-// console.log("hello duniya version 4")//yaha pe ham console .log se print karva rahe hai isme 
-// console.log("hello ji kaise ho sare")
- 
-// let a=15;
-// console.log(a);
+// console.log("hello shashank")
+// //object literal
+// let rectangle = {
+//     length: 1,
+//     breadth:2,
 
-
-// let b=10;
-// let c=20;
-// // console.log(b+c);
-// let lastName ="babbar"
-// console.log(lastName);
-
-// lastName=3;
-// console.log(lastName);
-
-// let person={
-//     firstName:"love",
-//     age:24
+//     draw: function(){
+//         console.log('drawing rectangle');
+//     }
 // };
 
-// console.log(person.age)
-
-// console.log()
-
-// let lastName=['love','rahul','sagar']
-// console.log(lastName[1]) 
-
-// console.log()
-
-// console.log(lastName[2])
-
-// let a=5;
-// let b=3;
-// console.log(a/b)
-
-// console.log(a*b)
-
-
-// let x=10
-// console.log(++x);
-
-
-// let y=20
-// console.log(++x)
-
-// console.log(3>=3);
-
-// let age=17;
-// let status=(age>=18)?'yes vote':'no vote';
-// console.log(status)
-
-
-
-// console.log(true || "shashank")
-
-
-// let marks=90;
-// if(marks>=90){
-//     console.log('A');
+//factory function is used here. for oops.
+// function createRectangle(len,bre){
+//     return rectangle = {
+//         length: len,
+//         breadth:bre,
+    
+//         draw: function(){
+//             console.log('drawing rectangle');
 // }
-// else if(marks>=80){
-//     console.log('B')
+//     };
+// }
+
+// let obj=createRectangle(5,8)//function call
+// console.log(obj);
+
+
+//constructor fuction->>Pascal notation follows.
+// function Rectangle(len,bre){
+//     this.length=len;
+//     this.breadth=bre;
+//     this.draw=function(){
+//         console.log('drawing');
+//     }
+// }
+// let rectangleObject = new Rectangle(4,6);//new space provide to the object
+// rectangleObject.colour='yellow';//adding a new property to the object
+// console.log(rectangleObject);
+// delete rectangleObject.colour;
+// console.log(rectangleObject);//delete the property in the object
+
+
+// let a={value:10};
+// let b=a;
+// a.value++;
+// console.log(a.value);
+// console.log(b.value);
+
+
+// let a={value:10};
+
+// function inc(a){
+//     a.value++;
+// }
+// inc(a);
+// console.log(a.value);//iske andar adress or refernced value jati hai.
+
+
+//usnig the for-in loops--->>>1.
+// let rectangle={
+//     length:2,
+//     breadth:4
+// };
+// for(let key in rectangle){
+//     console.log(key,rectangle[key]);//here we used the bracket notations
+// }
+
+//2nd example
+// let triangle={
+//     length:5,
+//     breadtgh:9
+
+// };
+// for(let key in triangle){
+//     console.log(key,triangle[key]);
+// }
+
+
+//checking the property is exsisits or not..
+// let triangle={
+//     length:5,
+//     breadtgh:9
+// };
+// if('length'in triangle){
+//     console.log('present');
 // }
 // else{
-//     console.log('E')
+//     console.log('Absent')
 // }
 
-// let num=3;
-// switch(num){
-// case 1: console.log('A');
-// break;
-// case 2: console.log('B');
-// break;
-// case 3: console.log('C');
-// break;
-// default: console.log('D');
+//object cloning 
+//1.
+
+// let src={
+//     a:10,
+//     b:20,
+//     c:30
+// };
+//  let dest={};
+// for(let key in src){
+//     dest[key]=src[key];
 // }
+// console.log(dest);
+
+//object clone 2nd method
+
+// let src={
+//     a:10,
+//     b:20,
+//     c:30
+// };
+
+// let dest = Object.assign({},src);
+// console.log(dest);
+// src.a++;
+// console.log(dest);
 
 
+//object clone 3rd method
 
-// for(let i=0; i<5;i=i+1)
-// {
-//     console.log(i);
-// }
-// for(let i=0; i<5;i=i+1)
-// {
-//     console.log(i);
-// }//for loop
+let src={
+        a:10,
+        b:20,
+        c:30
+    };
+    let dest={...src};
+    console.log(dest);
+    src.a++;
+    console.log(dest);
 
-// let i=0;
-// while(i<10)
-// {
-//     console.log(i);
-   
-// }
-// i++;
 
-// let x= 1;
-
-// while(x<=10){
-//  console.log("shashank"+x);
-//  x++
-
-// }//while loop
-
-let i=1;
-do
-{
-    console.log(i);
-    i++;
-}while(i<=10);
