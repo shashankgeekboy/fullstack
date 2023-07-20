9492a8785a154fe579cb13829a96026ca241324d
Shashank_geekboy <113760650+shashankgeekboy@users.noreply.github.com>
2023-07-21 02:20:09 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,32 @@
+// var a = 5;
+// var a = 'shashank'; 
+// var a=true//redeclaration in var 
+// console.log(a);  //var ko kitne baar bhi krlo
+
+
+// const u=12;
+// console.log(u);
+
+// let lastName='shashank';
+// console.log(lastName);
+// lastName=5;
+// console.log(lastName);
+
+//arithmatic operator.
+  let num=4;
+  let num1=2;
+  console.log(num+num1);
+  console.log(num-num1);
+  console.log(num*num1);
+  console.log(num/num1)
+
+  //comparison operator
+  console.log(2>4);
+  console.log(6>8);
+  console.log(3>=3);
+
+  let number = 2;
+  let str = '2';
+  console.log(number == str);
+  console.log(number === str);// strict equality
+
